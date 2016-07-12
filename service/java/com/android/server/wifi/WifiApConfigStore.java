751b8bf144759b79e06a4b70490ee6c3c26cdfe4
Raja M <raja1612.m@gmail.com>
2016-07-13 01:27:53 +0530

    Revert "Tethering: Turn off Wi-Fi Hotspot after inactivity (2/3)"
    This reverts commit 86ed3bc8c0fff3dd8aeae1d6493fc46a684df6f2.
    
    Change-Id: I3cf1f0afee87daaa9bfddef895ca6c2b01c9c9d8

--- service/java/com/android/server/wifi/WifiApConfigStore.java
+++ service/java/com/android/server/wifi/WifiApConfigStore.java
@@ -182,10 +182,6 @@ class WifiApConfigStore extends StateMachine {
             if (authType != KeyMgmt.NONE) {
                 config.preSharedKey = in.readUTF();
             }
-            // read in wifiApInactivityTimeout if bytes are available from in
-            if (in.available() != 0) {
-                config.wifiApInactivityTimeout = in.readLong();
-            }
 
             mWifiApConfig = config;
         } catch (IOException ignore) {
@@ -218,7 +214,6 @@ class WifiApConfigStore extends StateMachine {
             if(authType != KeyMgmt.NONE) {
                 out.writeUTF(config.preSharedKey);
             }
-            out.writeLong(config.wifiApInactivityTimeout);
         } catch (IOException e) {
             Log.e(TAG, "Error writing hotspot configuration" + e);
         } finally {
@@ -258,7 +253,6 @@ class WifiApConfigStore extends StateMachine {
             config.preSharedKey = randomUUID.substring(0, 8)
                     + randomUUID.substring(9, 13);
         }
-        config.wifiApInactivityTimeout = 0;
 
         sendMessage(WifiStateMachine.CMD_SET_AP_CONFIG, config);
     }
